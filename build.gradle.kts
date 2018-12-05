0d9f586195248826c3af3e2e030ed758faba57b6
YangDR <ydrml@hotmail.com>
2018-12-06 05:01:02 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+import org.jetbrains.dokka.gradle.DokkaTask
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+    repositories {
+        mavenCentral()
+        jcenter()
+    }
+
+    dependencies {
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin")
+    }
+}
+
+plugins {
+    kotlin("jvm") version "1.3.10"
+    id("org.jetbrains.dokka") version "0.9.16"
+}
+
+group = "org.mechdancer"
+version = "0.1.0-dev-1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+    testCompile("junit", "junit", "+")
+    compile(kotlin("reflect"))
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+task<Jar>("sourceJar") {
+    classifier = "sources"
+    from(sourceSets["main"].allSource)
+}
+
+task<Jar>("javadocJar") {
+    classifier = "javadoc"
+    from("$buildDir/javadoc")
+}
+
+tasks.withType<DokkaTask> {
+    outputFormat = "javadoc"
+    outputDirectory = "$buildDir/javadoc"
+}
+
+tasks["javadoc"].dependsOn("dokka")
+tasks["jar"].dependsOn("sourceJar")
+tasks["jar"].dependsOn("javadocJar")
