a290487dd18ffcfedbfbb0f0f02977b4a54e74cb
berberman <1793913507@qq.com>
2018-12-08 17:40:28 +0800

    抽出获取泛型类型函数

--- src/main/kotlin/org/mechdancer/dependency/Functions.kt
+++ src/main/kotlin/org/mechdancer/dependency/Functions.kt
@@ -1,16 +1,19 @@
 package org.mechdancer.dependency
 
+import java.lang.reflect.ParameterizedType
+import kotlin.reflect.KClass
+
 /** 找到一种 [C] 类型的依赖 */
 inline fun <reified C : Component> Iterable<Component>.get(): List<C> =
-    mapNotNull { it as? C }
+        mapNotNull { it as? C }
 
 /** 找到一种 [C] 类型的依赖 */
 inline fun <reified C : Component> Iterable<Component>.maybe(): C? =
-    get<C>().singleOrNull()
+        get<C>().singleOrNull()
 
 /** 找到一种 [C] 类型的依赖 */
 inline fun <reified C : Component> Iterable<Component>.must(): C =
-    maybe() ?: throw ComponentNotExistException(C::class)
+        maybe() ?: throw ComponentNotExistException(C::class)
 
 /** 向动态域添加新的依赖项 */
 operator fun DynamicScope.plusAssign(Component: Component) {
@@ -19,8 +22,18 @@ operator fun DynamicScope.plusAssign(Component: Component) {
 
 /** 构造动态域 */
 fun scope(block: DynamicScope.() -> Unit) =
-    DynamicScope().apply(block)
+        DynamicScope().apply(block)
 
 /** 构造映射浏览器 */
 internal fun <T, U> buildView(map: Map<T, U>) =
-    object : Map<T, U> by map {}
+        object : Map<T, U> by map {}
+
+/** 找到第一个上界为 [upper] 的泛型 */
+fun KClass<*>.firstGenericType(upper: KClass<*>) =
+        (java.genericSuperclass as? ParameterizedType)
+                ?.actualTypeArguments
+                ?.asSequence()
+                ?.mapNotNull { it as? Class<*> }
+                ?.find { t -> upper.java.isAssignableFrom(t) }
+                ?.kotlin
+                ?: throw RuntimeException("Unable to find component type.")
--- src/main/kotlin/org/mechdancer/dependency/unique/UniqueComponent.kt
+++ src/main/kotlin/org/mechdancer/dependency/unique/UniqueComponent.kt
@@ -1,7 +1,7 @@
 package org.mechdancer.dependency.unique
 
 import org.mechdancer.dependency.Component
-import java.lang.reflect.ParameterizedType
+import org.mechdancer.dependency.firstGenericType
 import kotlin.reflect.KClass
 import kotlin.reflect.full.safeCast
 
@@ -12,20 +12,13 @@ import kotlin.reflect.full.safeCast
  *   泛型 [T] 可保证此类型来自这个实现类
  */
 abstract class UniqueComponent<T : UniqueComponent<T>>
-    (type: KClass<T>? = null) : Component {
+(type: KClass<T>? = null) : Component {
 
-    val type = type
-        ?: (javaClass.genericSuperclass as? ParameterizedType)
-            ?.actualTypeArguments
-            ?.asSequence()
-            ?.mapNotNull { it as? Class<*> }
-            ?.find { t -> UniqueComponent::class.java.isAssignableFrom(t) }
-            ?.kotlin
-        ?: throw RuntimeException("Unable to find component type.")
+    val type = type ?: javaClass.kotlin.firstGenericType(UniqueComponent::class)
 
     override fun equals(other: Any?) =
-        this === other || type.safeCast(other) !== null
+            this === other || type.safeCast(other) !== null
 
     override fun hashCode() =
-        type.hashCode()
+            type.hashCode()
 }
