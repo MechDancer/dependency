e0f734518625de833e936f2ab8915f182dadf198
YdrMaster <ydrml@hotmail.com>
2021-08-10 13:45:38 +0800

    build: add publish actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
-plugins { kotlin("jvm") version "1.5.21" }
+plugins {
+    kotlin("jvm") version "1.5.21"
+    `maven-publish`
+}
 
 group = "org.mechdancer"
-version = "0.1.0-rc-4"
+version = "0.1.0-rc-5"
 
 repositories { mavenCentral() }
 dependencies {
@@ -17,142 +20,20 @@ java { withSourcesJar(); withJavadocJar() }
 tasks.withType<JavaCompile> { options.encoding = "UTF-8" }
 tasks.test { useJUnitPlatform() }
 
-//import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-//
-//plugins {
-//    kotlin("jvm") version "1.5.21" apply (true)
-//    id("org.jetbrains.dokka") version "0.10.0"
-//    id("com.jfrog.bintray") version "1.8.4"
-//    `maven-publish`
-//    `build-scan`
-//}
-//
-//buildScan {
-//    termsOfServiceUrl = "https://gradle.com/terms-of-service"
-//    termsOfServiceAgree = "yes"
-//    publishAlways()
-//}
-//
-//group = "org.mechdancer"
-//version = "0.1.0-rc-3"
-//
-//repositories {
-//    mavenCentral()
-//    jcenter()
-//}
-//dependencies {
-//    implementation(kotlin("stdlib-jdk8"))
-//    implementation(kotlin("reflect"))
-//
-//    testImplementation("junit", "junit", "+")
-//    testImplementation(kotlin("test-junit"))
-//}
-//tasks.withType<KotlinCompile> {
-//    kotlinOptions {
-//        jvmTarget = "1.8"
-//    }
-//}
-//
-//tasks.dokka {
-//    outputFormat = "javadoc"
-//    outputDirectory = "$buildDir/javadoc"
-//}
-//
-//val doc = tasks.register<Jar>("javadocJar") {
-//    group = JavaBasePlugin.DOCUMENTATION_GROUP
-//    description = "Assembles Kotlin docs with Dokka"
-//    archiveClassifier.set("javadoc")
-//    from(tasks.dokka)
-//}
-//
-//val sources = tasks.register<Jar>("sourcesJar") {
-//    group = JavaBasePlugin.BUILD_TASK_NAME
-//    description = "Creates sources jar"
-//    archiveClassifier.set("sources")
-//    from(sourceSets.main.get().allSource)
-//}
-//
-//val fat = tasks.register<Jar>("fatJar") {
-//    group = JavaBasePlugin.BUILD_TASK_NAME
-//    description = "Packs binary output with dependencies"
-//    archiveClassifier.set("all")
-//    from(sourceSets.main.get().output)
-//    from({
-//        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
-//    })
-//}
-//
-//tasks.register("allJars") {
-//    group = JavaBasePlugin.BUILD_TASK_NAME
-//    description = "Assembles all jars in one task"
-//    dependsOn(doc, sources, fat, tasks.jar)
-//}
-//
-//val rename = tasks.register("renamePomFile") {
-//    dependsOn(tasks.publishToMavenLocal)
-//    doLast {
-//        val path = "${buildDir.absolutePath}/publications/maven/"
-//        val old = File(path + "pom-default.xml")
-//        val f = File("$path${project.name}-$version.pom")
-//        old.renameTo(f)
-//    }
-//}
-//
-//tasks.bintrayUpload.configure {
-//    dependsOn(rename)
-//}
-//
-//bintray {
-//    user = "berberman"
-//    key = System.getenv("BintrayToken")
-//    setConfigurations("archives")
-//    val v = version.toString()
-//    val url = "https://github.com/MechDancer/dependency/"
-//    publish = true
-//    pkg.apply {
-//        name = project.name
-//        desc = "a lightweight dependency management tool."
-//        repo = "maven"
-//        userOrg = "mechdancer"
-//        githubRepo = "MechDancer/dependency"
-//        vcsUrl = "$url.git"
-//        issueTrackerUrl = "$url/issues"
-//        publicDownloadNumbers = true
-//        setLicenses("WTFPL")
-//        version.apply {
-//            name = v
-//            vcsTag = v
-//            websiteUrl = "$url/releases/tag/$v"
-//        }
-//    }
-//}
-//
-//publishing {
-//
-//    repositories {
-//        maven("$buildDir/repo")
-//        maven {
-//            name = "GitHubPackages"
-//            url = uri("https://maven.pkg.github.com/MechDancer/dependency")
-//            credentials {
-//                username = "MechDancerProject"
-//                password = System.getenv("GitHubToken")
-//            }
-//        }
-//    }
-//
-//
-//    publications {
-//        create<MavenPublication>("maven") {
-//            from(components["java"])
-//        }
-//    }
-//}
-//
-//artifacts {
-//    add("archives", tasks.jar)
-//    add("archives", fat)
-//    add("archives", sources)
-//    add("archives", doc)
-//    add("archives", File("${buildDir.absolutePath}/publications/maven/${project.name}-$version.pom"))
-//}
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/mechdancer/dependency")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
